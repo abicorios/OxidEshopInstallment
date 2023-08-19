67d23790c57c0af44657c3ae1280e2bce729269a
abicorios <yaroslav.moroz91@gmail.com>
2023-08-19 03:11:32 +0300

    Add the metadata file

---
+++ metadata.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'abicorios_installment',
+    'title'       => [
+        'de' => 'Abicorios - Ratenzahlung',
+        'en' => 'Abicorios - Installment',
+    ],
+    'description' => [
+        'de' => 'Ratenzahlung für die Produkte einrichten.',
+        'en' => 'Set up installments for the products.',
+    ],
+    'thumbnail'   => '',
+    'version'     => '1.0',
+    'author'      => 'Abicorios',
+    'url'         => 'https://github.com/abicorios/OxidEshopInstallment',
+    'email'       => 'yaroslav.moroz91@gmail.com',
+    'extend'      => [
+    ],
+    'blocks'      => [
+    ],
+    'settings'    => [
+    ],
+];
