2a4ec0fe6c623aa58084cb061628cd3e95a5c130
abicorios <yaroslav.moroz91@gmail.com>
2023-08-19 07:47:56 +0300

    Add the OXNUMBER_OF_INSTALLMENT_MONTHS column to the oxarticles table

--- migration/data/Version20230819041414.php
+++ migration/data/Version20230819041414.php
@@ -7,9 +7,6 @@ namespace OxidEsales\abicorios\OxidEshopInstallment;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230819041414 extends AbstractMigration
 {
     public function getDescription() : string
@@ -19,13 +16,11 @@ final class Version20230819041414 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE `oxarticles` ADD `OXNUMBER_OF_INSTALLMENT_MONTHS` TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT "Number of installment months"');
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE `oxarticles` DROP COLUMN `OXNUMBER_OF_INSTALLMENT_MONTHS`');
     }
 }
