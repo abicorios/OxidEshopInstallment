1058fca2c74d357e653809610145a24e9e006411
abicorios <yaroslav.moroz91@gmail.com>
2023-08-20 07:09:36 +0300

    Protect division by zero, to improve stability

--- Model/Article.php
+++ Model/Article.php
@@ -12,6 +12,9 @@ class Article extends Article_parent
         $price = $this->oxarticles__oxprice->value;
         $prepayment = $this->oxarticles__abicorios_installment_prepayment->value;
         $months = $this->oxarticles__abicorios_installment_number_of_months->value;
+        if ($months == 0) {
+            return $price - $prepayment;
+        }
         $monthlyInstallmentPrice = ($price - $prepayment) / $months;
         $monthlyInstallmentPrice = round($monthlyInstallmentPrice, 2);
         return $monthlyInstallmentPrice;
