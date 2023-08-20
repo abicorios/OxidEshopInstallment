6b5ff8aa6f3f173fef057321b9795b50825b1e57
abicorios <yaroslav.moroz91@gmail.com>
2023-08-20 17:22:20 +0300

    Set the prepayment data type to the double unsigned

--- migration/data/Version20230819031800.php
+++ migration/data/Version20230819031800.php
@@ -16,7 +16,7 @@ final class Version20230819031800 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $this->addSql('ALTER TABLE `oxarticles` ADD `ABICORIOS_INSTALLMENT_PREPAYMENT` DOUBLE NOT NULL DEFAULT 0 COMMENT "Article prepayment"');
+        $this->addSql('ALTER TABLE `oxarticles` ADD `ABICORIOS_INSTALLMENT_PREPAYMENT` DOUBLE UNSIGNED NOT NULL DEFAULT 0 COMMENT "Article prepayment"');
 
     }
 
