47f73e189b1b2be9b5627ac8488e94005a604d85
abicorios <yaroslav.moroz91@gmail.com>
2023-08-20 14:11:53 +0300

    Add the Deutsch translation

---
+++ translations/de/installment_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$sLangName = 'Deutsch';
+$iLangNr = 0;
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = [
+    'charset' => 'UTF-8',
+    'ABICORIOS_INSTALLMENT_TITLE' => 'Zahlen Sie später in mehreren Monaten in Raten',
+    'ABICORIOS_INSTALLMENT_PRICE' => 'Preis:',
+    'ABICORIOS_INSTALLMENT_PREPAYMENT' => 'Vorauszahlung:',
+    'ABICORIOS_INSTALLMENT_MONTHLY' => '/ Monat',
+    'ABICORIOS_INSTALLMENT_INSTALLMENTS' => 'Raten',
+];
---
+++ views/admin/de/installment_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$sLangName = 'Deutsch';
+$iLangNr = 0;
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = [
+    'charset' => 'UTF-8',
+    'ABICORIOS_INSTALLMENT_PREPAYMENT' => 'Vorauszahlung',
+    'ABICORIOS_INSTALLMENT_NUMBER_OF_MONTHS' => 'N Ratenmonate',
+    'HELP_ABICORIOS_INSTALLMENT_NUMBER_OF_MONTHS' => 'Anzahl der Ratenmonate',
+];
