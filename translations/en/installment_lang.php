68ff45de55207b5b05d40f216631a19185ffd829
abicorios <yaroslav.moroz91@gmail.com>
2023-08-20 03:24:42 +0300

    Add the banner and the popup modal window

---
+++ translations/en/installment_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$sLangName = 'English';
+$iLangNr = 1;
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = [
+    'charset' => 'UTF-8',
+    'ABICORIOS_INSTALLMENT_TITLE' => 'Pay later in several months in installments',
+    'ABICORIOS_INSTALLMENT_SUBTITLE' => 'Buy today - no down payment',
+    'ABICORIOS_INSTALLMENT_PRICE' => 'Price:',
+    'ABICORIOS_INSTALLMENT_PREPAYMENT' => 'Prepayment:',
+    'ABICORIOS_INSTALLMENT_MONTHLY' => '/ month',
+    'ABICORIOS_INSTALLMENT_INSTALLMENTS' => 'installments',
+];
